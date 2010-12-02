dd53a3544ff3ddd517b1a4ed606fe3ffbcbe956d
Ben Tillman <bstillman@crossroadsint.org>
2010-12-02 13:05:22 +0800

    Added support for includes

--- lib/crm_meta_search/controllers.rb
+++ lib/crm_meta_search/controllers.rb
@@ -2,19 +2,28 @@
 
   controller = (model.name.pluralize + 'Controller').constantize
   controller.class_eval %Q{
-    skip_before_filter :require_user, :only => :meta_search
+    skip_before_filter :require_user, :only => [:meta_search]
+    before_filter :require_application, :only => :meta_search
 
     def meta_search
       @search = #{model}.metasearch(params[:search])
       @only = params[:only] || [:id, :name]
       @limit = params[:limit] || 10
 
-      @results = @search.all(:limit => @limit)
+      @results = @search.all(:include => params[:include], :limit => @limit)
 
       respond_to do |format|
         format.json { render :json => @results.to_json(:only => [false], :methods => @only) }
         format.xml  { render :xml => @results.to_xml(:only => [false], :methods => @only) }
       end
+    end
+
+    def current_application_session
+      @current_application_session ||= ApplicationSession.find
+    end
+
+    def require_application
+      current_application_session.present?
     end
   }
 end
