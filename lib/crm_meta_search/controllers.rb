7ca26fb70d0e2b6ca53172aabbb086677e117ee3
Nathan B <nathan.f77@gmail.com>
2011-02-09 17:19:43 +0800

    Refactored horrible merge case logic, added lots of comments, fixed it all up

--- lib/crm_meta_search/controllers.rb
+++ lib/crm_meta_search/controllers.rb
@@ -9,21 +9,37 @@
     def meta_search
       alias_id_hash = {}
       if params[:search][:id_in]
+        # Sanitizes params search ids, replaces deleted / merged contact ids
+        # with current ids.
         alias_id_hash = ContactAlias.ids_with_alias(params[:search][:id_in])
-        params[:search][:id_in] = alias_id_hash.values
+        params[:search][:id_in] = alias_id_hash.values.uniq
       end
 
+      # Find all records that match our sanitized id set.
       @search = klass.metasearch(params[:search])
       @only = params[:only] || [:id, :name]
       @limit = params[:limit] || 10
 
-      @results = @search.all(:include => params[:include], :limit => @limit)
-      alias_id_hash.each do |asset_id, alias_id|
-        if result = @results.detect { |r| r.id == alias_id.to_i } and Contact.find_by_id(asset_id)
-          result.id = asset_id # Associate merged
+      @search = @search.all(:include => params[:include], :limit => @limit)
+
+      @results = []
+      # Iterate through search ids and the current asset id (for merged assets).
+      alias_id_hash.each do |search_id, current_id|
+        # If the search results contain the search id, add to results.
+        if record = @search.detect{|record| record.id == search_id.to_i }
+          @results << record.dup
+        # Else, if the search results don't contain the search id,
+        # but the search results do contain the current id, change the current
+        # id back to the old id and add to results.
+        elsif record = @search.detect{|record| record.id == current_id.to_i }
+          record_with_old_id = record.dup
+          record_with_old_id.id = search_id
+          @results << record_with_old_id
+        # Finally, if no record exists (asset has been destroyed, not merged),
+        # create a new 'deleted contact' for the asset.
         else
           contact = Contact.new(:first_name => "[Deleted", :last_name => "Contact]")
-          contact.id = asset_id
+          contact.id = search_id
           @results << contact
         end
       end
@@ -56,3 +72,4 @@
 
   end
 end
+
