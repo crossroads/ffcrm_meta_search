426e7d439dc412478085bbeab0658781bca94ab4
Nathan B <nathan.f77@gmail.com>
2011-02-10 16:48:38 +0800

    Fixed awful bug where search wasnt working for single query

--- lib/crm_meta_search/controllers.rb
+++ lib/crm_meta_search/controllers.rb
@@ -22,26 +22,32 @@
 
       @search = @search.all(:include => params[:include], :limit => @limit)
 
-      @results = []
-      # Iterate through search ids and the current asset id (for merged assets).
-      alias_id_hash.each do |search_id, current_id|
-        # If the search results contain the search id, add to results.
-        if record = @search.detect{|record| record.id == search_id.to_i }
-          @results << record.dup
-        # Else, if the search results don't contain the search id,
-        # but the search results do contain the current id, change the current
-        # id back to the old id and add to results.
-        elsif record = @search.detect{|record| record.id == current_id.to_i }
-          record_with_old_id = record.dup
-          record_with_old_id.id = search_id
-          @results << record_with_old_id
-        # Finally, if no record exists (asset has been destroyed, not merged),
-        # create a new 'deleted contact' for the asset.
-        else
-          contact = Contact.new(:first_name => "[Deleted", :last_name => "Contact]")
-          contact.id = search_id
-          @results << contact
+      # If we are doing an id collection search
+      if params[:search][:id_in]
+        @results = []
+        # Iterate through search ids and the current asset id (for merged assets).
+        alias_id_hash.each do |search_id, current_id|
+          # If the search results contain the search id, add to results.
+          if record = @search.detect{|record| record.id == search_id.to_i }
+            @results << record.dup
+          # Else, if the search results don't contain the search id,
+          # but the search results do contain the current id, change the current
+          # id back to the old id and add to results.
+          elsif record = @search.detect{|record| record.id == current_id.to_i }
+            record_with_old_id = record.dup
+            record_with_old_id.id = search_id
+            @results << record_with_old_id
+          # Finally, if no record exists (asset has been destroyed, not merged),
+          # create a new 'deleted contact' for the asset.
+          else
+            contact = Contact.new(:first_name => "[Deleted", :last_name => "Contact]")
+            contact.id = search_id
+            @results << contact
+          end
         end
+      else
+        # If we are just searching for a single record
+        @results = @search
       end
 
       respond_to do |format|
