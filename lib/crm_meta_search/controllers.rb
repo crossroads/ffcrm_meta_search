77eacf13631a0da7efb6843ed6b157fc19a19235
Steve Kenworthy <steveyken@gmail.com>
2011-12-15 17:57:05 +0800

    Fixed bug where contact id was not included upon record duplication. And remove various assumptions about Contact.

--- lib/crm_meta_search/controllers.rb
+++ lib/crm_meta_search/controllers.rb
@@ -9,7 +9,7 @@
     def meta_search
       alias_id_hash = {}
       if params[:search][:id_in]
-        # Sanitizes params search ids, replaces deleted / merged contact ids
+        # Sanitizes params search ids, replaces deleted / merged object ids
         # with current ids.
         alias_id_hash = ContactAlias.ids_with_alias(params[:search][:id_in])
         params[:search][:id_in] = alias_id_hash.values.uniq
@@ -22,14 +22,15 @@
 
       @search = @search.all(:include => params[:include], :limit => @limit)
 
-      # If we are doing an id collection search
       if params[:search][:id_in]
         @results = []
         # Iterate through search ids and the current asset id (for merged assets).
         alias_id_hash.each do |search_id, current_id|
           # If the search results contain the search id, add to results.
           if record = @search.detect{|record| record.id == search_id.to_i }
-            @results << record.dup
+            r = record.dup
+            r.id = search_id
+            @results << r
           # Else, if the search results don't contain the search id,
           # but the search results do contain the current id, change the current
           # id back to the old id and add to results.
@@ -37,12 +38,20 @@
             record_with_old_id = record.dup
             record_with_old_id.id = search_id
             @results << record_with_old_id
-          # Finally, if no record exists (asset has been destroyed, not merged),
-          # create a new 'deleted contact' for the asset.
+          # Finally, if no record exists then object has been destroyed, not merged
+          # so return a new 'deleted' object.
           else
-            contact = Contact.new(:first_name => "[Deleted", :last_name => "Contact]")
-            contact.id = search_id
-            @results << contact
+            deleted_params = case klass.name
+              when 'Account', 'Campaign', 'Opportunity', 'Task'
+                {:name => "[Deleted #{klass.name}]"}
+              when 'Contact', 'Lead'
+                {:first_name => "[Deleted", :last_name => "#{klass.name}]"}
+              else
+                {}
+              end
+            obj = klass.new(deleted_params)
+            obj.id = search_id
+            @results << obj
           end
         end
       else
